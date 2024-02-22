ad50cc2e21c79c77f3b29eee8ab6bf87d6448cf8
Bill Maxwell <cloudnautique@users.noreply.github.com>
2024-02-22 11:30:43 -0700

    Initial commit
    Signed-off-by: Bill Maxwell <cloudnautique@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,40 @@
+# gptscript JSON query tool
+
+This tool is a simple command line tool to query json documents using jq. It is written in javascript and uses the node-jq package to install and run jq.
+
+## Installation
+
+```bash
+git clone github.com/gptscript/json-query
+cd json-query
+npm install
+```
+
+## CLI Usage
+
+To test this command out directly you can run:
+
+```bash
+node jq.js --query '.' --jsonpath ./package.json
+```
+
+this will output the local package.json file.
+
+## gptscript usage
+
+To use this tool in a gptscript you can use the following code:
+
+```gptscript
+tools: ./tool.gpt
+
+What is the version specified in the package.json file
+```
+
+A more complex example:
+
+```gptscript
+tools: ./tool.gpt,sys.download
+
+download https://raw.githubusercontent.com/bahamas10/css-color-names/master/css-color-names.json
+find out the css values for darkviolet and the color blue
+```
---
+++ jq.js
@@ -0,0 +1,33 @@
+const jq = require('node-jq');
+// Simple argument parsing function
+function parseArgs() {
+    const args = {};
+    process.argv.slice(2).forEach((val, index, array) => {
+        if (val.startsWith('--')) {
+            const argName = val.substring(2);
+            // Check if we are not at the last element and the next element doesn't start with '--'
+            if (index < array.length - 1 && !array[index + 1].startsWith('--')) {
+                args[argName] = array[index + 1];
+            }
+        }
+    });
+    return args;
+}
+
+const args = parseArgs();
+
+// Fallback to default values if arguments not provided
+const filter = args.filter || '.';
+const jsonPath = args.jsonpath;
+const options = {
+    input: 'file',
+    output: 'json'
+};
+
+jq.run(filter, jsonPath, options)
+    .then((output) => {
+        console.log(output);
+    })
+    .catch((err) => {
+        console.error(err);
+    });
