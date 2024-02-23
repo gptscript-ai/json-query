960ea1695825876fb88b5061fa1083458d162824
Bill Maxwell <cloudnautique@users.noreply.github.com>
2024-02-23 16:52:30 -0700

    handle json docs containing an object per line
    Large datasets in JSON sometimes come in files with
    an object entry per line. When looking for the schema, the tool
    could quickly list enough entries to fill the context window.
    
    This updates the tool to try and process larger files with
    the slurp functionality and treat all entries in the file as items
    in an array. This lets lenght functions work, and grabbing a single
    entry.
    
    Signed-off-by: Bill Maxwell <cloudnautique@users.noreply.github.com>

--- jq.js
+++ jq.js
@@ -7,6 +7,7 @@ function parseArgs() {
             const argName = val.substring(2);
             // Check if we are not at the last element and the next element doesn't start with '--'
             if (index < array.length - 1 && !array[index + 1].startsWith('--')) {
+                // Directly assign the following value to the argument
                 args[argName] = array[index + 1];
             }
         }
@@ -21,7 +22,8 @@ const filter = args.filter || '.';
 const jsonPath = args.jsonpath;
 const options = {
     input: 'file',
-    output: 'json'
+    output: 'json',
+    slurp: args.optionSlurp === 'true'
 };
 
 jq.run(filter, jsonPath, options)
